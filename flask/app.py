5904c3665dfb6f1d29eeff56fbc092f417294c4b
ZenVariable <zhen.huang.o@outlook.com>
2025-05-23 14:59:03 +0800

    Create items by form

--- flask/app.py
+++ flask/app.py
@@ -33,8 +33,9 @@
 #     return render_template("index.html", name = name, movies = movies)
 
 
-
+# Database embedded
 from flask import Flask, render_template, url_for
+from flask import request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import os 
 
@@ -69,8 +70,19 @@ def inject_user():
     user = User.query.first()
     return dict(user = user)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year = year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template('index.html', movies = movies)
 
@@ -103,4 +115,34 @@ def forge():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie = movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+
 
